2338778eaff4431285e6b13600a6e4c14965f1bf
rodrigoestrellac <rodrigoestrellac@gmail.com>
2022-09-21 00:44:30 -0300

    updated js and css

--- js/movement.js
+++ js/movement.js
@@ -1,4 +1,39 @@
-const robot = document.querySelector('.robot');
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+  const robot = document.querySelector('.robot');
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+    const val = scrolled * 0.3;
+    const pcMoved = Math.min(0.10 * val, 43);
+    robot.style.transform = `translateX(${-pcMoved}%)`;
+    /*robot.style.marginTop = `${0.1 * val}%`;
+    robot.style.transform = `translateY(${0.45 * val}%)`;
+    robot.style.borderRadius = `${0.025 * val}%`;
+    robot.style.height = `${0.1 * val}px`;
+    robot.style.width = `${0.1 * val}px`;*/
+});
+
+const wine = document.querySelector('.wine');
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+    const val = scrolled * 0.3;
+    const pcMoved = Math.min(0.10 * val, 43);
+    console.log(val*0.2);
+    wine.style.transform = `translateX(${pcMoved}%)`;
+    /*wine.style.marginTop = `${0.05 * val}%`;
+    robot.style.transform = `translateY(${0.45 * val}%)`;
+    robot.style.borderRadius = `${0.025 * val}%`;
+    robot.style.height = `${0.1 * val}px`;
+    robot.style.width = `${0.1 * val}px`;*/
+});
+
+  
+} else {
+  const robot = document.querySelector('.robot');
 
 window.addEventListener('scroll', () => {
     const scrolled = window.pageYOffset;
@@ -26,6 +61,9 @@ window.addEventListener('scroll', () => {
     robot.style.height = `${0.1 * val}px`;
     robot.style.width = `${0.1 * val}px`;*/
 });
+
+}
+
 
 /*const square = document.querySelector('#proyectos > div.proyectos-img > div.square');
 
