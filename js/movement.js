ae77d9666a056cac0ac621897ada82098345003d
rodrigoestrellac <rodrigoestrellac@gmail.com>
2023-01-30 16:03:13 -0300

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -1,9 +1,56 @@
 // Create a media condition that targets viewports at least 768px wide
-const mediaQuery = window.matchMedia('(max-width: 1024px)')
+const mediaQuery = window.matchMedia('(max-width: 1100px)')
 // Check if the media query is true
 if (mediaQuery.matches) {
+  const mediaQuery = window.matchMedia('(max-width: 680px)')
+    // Check if the media query is true
+    if (mediaQuery.matches) {
+      
+    } else {
+      const robot = document.querySelector('.robot');
+    
+    window.addEventListener('scroll', () => {
+        const scrolled = window.pageYOffset;
+        const val = scrolled * 0.3;
+        const pcMoved = Math.min(0.05 * val, 43);
+        robot.style.transform = `translateY(${pcMoved}%)`;
+        /*robot.style.marginTop = `${0.1 * val}%`;
+        robot.style.transform = `translateY(${0.45 * val}%)`;
+        robot.style.borderRadius = `${0.025 * val}%`;
+        robot.style.height = `${0.1 * val}px`;
+        robot.style.width = `${0.1 * val}px`;*/
+    });
+    
+    const shopperRobot = document.querySelector('.preciosClaros');
+    
+    window.addEventListener('scroll', () => {
+        const scrolled = window.pageYOffset;
+        const val = scrolled * 0.3;
+        const pcMoved = Math.min(0.025 * val, 43);
+        shopperRobot.style.transform = `translateX(${-pcMoved}%)`;
+        /*robot.style.marginTop = `${0.1 * val}%`;
+        robot.style.transform = `translateY(${0.45 * val}%)`;
+        robot.style.borderRadius = `${0.025 * val}%`;
+        robot.style.height = `${0.1 * val}px`;
+        robot.style.width = `${0.1 * val}px`;*/
+    });
+    
+    const wine = document.querySelector('.wine');
+    
+    window.addEventListener('scroll', () => {
+        const scrolled = window.pageYOffset;
+        const val = scrolled * 0.3;
+        const pcMoved = Math.min(0.025 * val, 43);
+        /*console.log(val*0.2);*/
+        wine.style.transform = `translateX(${pcMoved}%)`;
+        /*wine.style.marginTop = `${0.05 * val}%`;
+        robot.style.transform = `translateY(${0.45 * val}%)`;
+        robot.style.borderRadius = `${0.025 * val}%`;
+        robot.style.height = `${0.1 * val}px`;
+        robot.style.width = `${0.1 * val}px`;*/
+    });
+    }
 
-  
 } else {
   const robot = document.querySelector('.robot');
 
